6d3b969af53886b9852420035e31572735152133
arnau-guell <163121052+arnau-guell@users.noreply.github.com>
2024-06-10 13:44:14 +0200

    Fixed tabs

--- 3-bidsqc/play_anat_gifs.sh
+++ 3-bidsqc/play_anat_gifs.sh
@@ -26,13 +26,13 @@ todo=0
 
 for file_path in $ROOT/*; do
 
-	file=$(basename $file_path)
+    file=$(basename $file_path)
 
-	if [[ $file == *"checked"* ]]; then
-  		((done=done+1))
-	else
-		((todo=todo+1))
-	fi
+    if [[ $file == *"checked"* ]]; then
+    ((done=done+1))
+    else
+        ((todo=todo+1))
+    fi
 
 done
 
@@ -47,50 +47,50 @@ declare -A quality_array=(["T1"]="1 2A 2B 3 4 EXCLUDED" ["T2"]="1 2 3 4 EXCLUDED
 n=0
 for file_path in $ROOT/*; do
 
-	file=$(basename $file_path)
-	suj=$(echo $file | grep -oP '(?<=sub-)[^_]+')
-	ses=$(echo $file | grep -oP '(?<=ses-)[^_]+')
-	run=$(echo $file | grep -oP '(?<=run-)[^_]+')
-	imgtype=$(echo $file | grep -oP '(?<=_)[^_]+(?=\.)')
-	declare -a qc_values=(${quality_array[$imgtype]})
-	if [[ $file == *"checked"* ]]; then
-  		echo "Subject $suj already checked"
-	else
-		echo
-		echo "####"
-		echo
-		echo "Checking subject $suj run $run..."
-		
-		#Opening gif
-		eom --fullscreen $file_path
-		
-		#Asking for rating
-	    read -p 'Rate quality: ' quality
-        quality="${quality//"."/}"
-	    while ! echo ${qc_values[@]} | grep -i -w -q ${quality} ; do
-	        read -p 'Invalid rating value. Please rate quality with a valid value: ' quality
-	        quality="${quality//"."/}"
-	    done
-	    
-	    read -p "Comment here (press ENTER to skip): " comment
-	    #Writing rate
-	    if [ ! -f $TXT_QUALITY_FILE ]; then
-	        echo "subject,run,rating_value,comment" >> $TXT_QUALITY_FILE
-	    fi
-	    echo "$suj,"$run",${quality^^},\"$comment\"" >> $TXT_QUALITY_FILE
+    file=$(basename $file_path)
+    suj=$(echo $file | grep -oP '(?<=sub-)[^_]+')
+    ses=$(echo $file | grep -oP '(?<=ses-)[^_]+')
+    run=$(echo $file | grep -oP '(?<=run-)[^_]+')
+    imgtype=$(echo $file | grep -oP '(?<=_)[^_]+(?=\.)')
+    declare -a qc_values=(${quality_array[$imgtype]})
+    if [[ $file == *"checked"* ]]; then
+        echo "Subject $suj already checked"
+    else
+        echo
+        echo "####"
+        echo
+        echo "Checking subject $suj run $run..."
 
-	    #Marking as checked by changing filename
-	    if [ -z "$ses" ]; then
+        #Opening gif
+        eom --fullscreen $file_path
+
+        #Asking for rating
+        read -p 'Rate quality: ' quality
+        quality="${quality//"."/}"
+        while ! echo ${qc_values[@]} | grep -i -w -q ${quality} ; do
+            read -p 'Invalid rating value. Please rate quality with a valid value: ' quality
+            quality="${quality//"."/}"
+        done
+
+        read -p "Comment here (press ENTER to skip): " comment
+        #Writing rate
+        if [ ! -f $TXT_QUALITY_FILE ]; then
+            echo "subject,run,rating_value,comment" >> $TXT_QUALITY_FILE
+        fi
+        echo "$suj,"$run",${quality^^},\"$comment\"" >> $TXT_QUALITY_FILE
+
+        #Marking as checked by changing filename
+        if [ -z "$ses" ]; then
             mv $ROOT/sub-${suj}_run-${run}_${imgtype}.gif $ROOT/sub-${suj}_run-${run}_${imgtype}_checked.gif
         else
             mv $ROOT/sub-${suj}_ses-${ses}_run-${run}_${imgtype}.gif $ROOT/sub-${suj}_ses-${ses}_run-${run}_${imgtype}_checked.gif
         fi
-	    
-		((n=n+1))
-		if [[ $n == 10 ]]; then
-			read -p "Take a breath; 10 checkings in a row. Press any key to continue" trashkey
-			n=0
-		fi
-	fi
+
+        ((n=n+1))
+        if [[ $n == 10 ]]; then
+            read -p "Take a breath; 10 checkings in a row. Press any key to continue" trashkey
+            n=0
+        fi
+    fi
 
 done
