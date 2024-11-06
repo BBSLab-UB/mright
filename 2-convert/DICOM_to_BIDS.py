5b01afe8e986d51f3f8b5168de56b10e8592d16e
rachmorse <86972143+rachmorse@users.noreply.github.com>
2024-11-06 17:12:00 +0100

    Added feature separate import/outport BIDS folders

--- 2-convert/DICOM_to_BIDS.py
+++ 2-convert/DICOM_to_BIDS.py
@@ -29,7 +29,8 @@ def main():
     # Input paths
     meta_create()
     dicoms_path = meta_func("dicom", "Enter the path to the DICOMs folder")  # Path to DICOM directories
-    bids_path = meta_func("bids_in", "Enter the path to the BIDS folder")  # Path to BIDS directory
+    bids_path = meta_func("bids_in", "Enter the path to the (shared) BIDS folder")  # Path to BIDS directory
+    temp_bids_path = meta_func("bids_in", "Enter the path to the temporary (local) BIDS output folder")  
 
     heuristic_file_path = meta_func("heuristic", "your heuristic file path") # Path to heuristic file
     ses = meta_func("ses", "your session label", ispath=False) # Timepoint (session) label
@@ -54,25 +55,22 @@ def main():
     else:
         bids = [s[4:] for s in os.listdir(bids_path) if ((s[:4] == "sub-"))]
 
-    # Clean DICOM folders and ensure format consistency
-    dicoms_in_list_clean = {f"sub-{re.sub(r'[^a-zA-Z0-9]', '', sub)}" for sub in dicoms_folders}
-
     # Identify subjects needing processing
-    todo_dicoms = {sub for sub in dicoms_in_list_clean if sub not in bids}
+    todo_dicoms = {sub for sub in dicoms_folders if sub not in bids}
 
     # Identify subjects that do not need processing
-    intersection_bids_list = dicoms_in_list_clean.intersection(bids)
+    intersection_bids_list = dicoms_folders.intersection(bids)
     
     # If there are no subjects in both dicoms_folders and bids_path
-    if intersection_bids_list == set():                                             # If there isn't any subject in both list and bids_path
+    if intersection_bids_list == set():                                             
         todo_dicoms = dicoms_folders
 
     # If there are subjects in both dicoms_folders and bids_path
-    while (intersection_bids_list != set()) is True:                                # Some subject is both in list and bids_path
+    while (intersection_bids_list != set()) is True:                                
         overwrite_bids = input(str(intersection_bids_list) +
                             " already in BIDS directory, do you want to overwrite? (Y/N) ").upper()
         # No overwriting: todo_dicoms = dicoms in list not in bids_path
-        if overwrite_bids == "N":                                                  # No overwriting: todo_dicoms = dicoms in list not in bids_path
+        if overwrite_bids == "N":                                                 
             todo_dicoms = []
             for sub in dicoms_folders:
                 sub_clean = re.sub(r'[^a-zA-Z0-9]', '', sub)
@@ -82,9 +80,9 @@ def main():
             intersection_bids_list = set()
 
         # Overwriting: delete BIDS in conflict, convert the entire list    
-        elif overwrite_bids == "Y":                                                # Overwriting: delete BIDS in conflict, convert the entire list
+        elif overwrite_bids == "Y":                                                
             if use_sessions == True:
-                for dicom_id in dicoms_in_list_clean:
+                for dicom_id in dicoms_folders:
                     if os.path.exists(os.path.join(bids_path, "sub-{}".format(dicom_id), ses_path)):
                         shutil.rmtree(os.path.join(bids_path, "sub-{}".format(dicom_id), ses_path))
                         print("INFO: " + os.path.join(bids_path, "sub-{}".format(dicom_id), ses_path) + " will be overwritten.")
@@ -93,7 +91,7 @@ def main():
                         print("INFO: " + os.path.join(bids_path, ".heudiconv", dicom_id, ses_path) + " will be overwritten.")
             
             else:            
-                for dicom_id in dicoms_in_list_clean:
+                for dicom_id in dicoms_folders:
                     if os.path.exists(os.path.join(bids_path, "sub-{}".format(dicom_id))):
                         shutil.rmtree(os.path.join(bids_path, "sub-{}".format(dicom_id)))
                         print("INFO: " + os.path.join(bids_path, "sub-{}".format(dicom_id)) + " will be overwritten.")
@@ -113,7 +111,7 @@ def main():
     for subj in todo_dicoms:
         try:
             subj_clean = re.sub(r'[^a-zA-Z0-9]', '', subj)
-            subj_path = os.path.join(bids_path, f"sub-{subj_clean}")
+            subj_path = os.path.join(temp_bids_path, f"sub-{subj_clean}")
             if not os.path.exists(subj_path):
                 os.mkdir(subj_path)
             subdir_list = [subdir for subdir in os.listdir(subj_path) if os.path.isdir(os.path.join(subj_path, subdir))]
@@ -125,7 +123,7 @@ def main():
                 if subdir_list:
                     subdir_check = [ses_subdir for ses_subdir in subdir_list if "ses-" in ses_subdir[:4]]
                     if subdir_check != subdir_list:
-                        with open(os.path.join(bids_path, "error_heudiconv.txt"), "a") as f:
+                        with open(os.path.join(temp_bids_path, "error_heudiconv.txt"), "a") as f:
                             print(f"WARNING: Subject {subj} has been skipped due to session hierarchy issues. Logged in error_heudiconv.txt")
                             f.write(str(datetime.datetime.now()) + "\t" + subj + " session hierarchy issue\n")
                         continue
@@ -134,7 +132,7 @@ def main():
                     command = "heudiconv -d "+ os.path.join(dicoms_path, "TP2", "{subject}", "*", "*") + " -o "+ bids_path +" -f "+ heuristic_file_path +" -s "+ subj + " -ss "+ ses +" -c dcm2niix -b --minmeta --overwrite --grouping custom"
                     os.system(command)
                 else:
-                    with open(os.path.join(bids_path, "error_heudiconv.txt"), "a") as f:
+                    with open(os.path.join(temp_bids_path, "error_heudiconv.txt"), "a") as f:
                         print(f"WARNING: Subject {subj} was previously processed and will be skipped. Logged in error_heudiconv.txt")
                         f.write(str(datetime.datetime.now()) + "\t" + subj + " already processed\n")
             
@@ -145,39 +143,39 @@ def main():
                 if subdir_list:
                     subdir_check = [ses_subdir for ses_subdir in subdir_list if "ses-" not in ses_subdir[:4]]
                     if subdir_check != subdir_list:
-                        with open(os.path.join(bids_path, "error_heudiconv.txt"), "a") as f:
+                        with open(os.path.join(temp_bids_path, "error_heudiconv.txt"), "a") as f:
                             print(f"WARNING: Subject {subj} has been skipped due to incorrect session structure. Logged in error_heudiconv.txt")
                             f.write(str(datetime.datetime.now()) + "\t" + subj + " incorrect session structure\n")
                         continue
-                if f"sub-{subj_clean}" not in os.listdir(bids_path) or not subdir_list:
+                if f"sub-{subj_clean}" not in os.listdir(temp_bids_path) or not subdir_list:
                     print(f"Starting subject {subj} conversion")
                     command = "heudiconv -d "+ os.path.join(dicoms_path, "TP2", "{subject}", "*", "*") + " -o "+ bids_path +" -f "+ heuristic_file_path +" -s "+ subj +" -c dcm2niix -b --minmeta --overwrite --grouping custom"
                     os.system(command)
                 else:
-                    with open(os.path.join(bids_path, "error_heudiconv.txt"), "a") as f:
+                    with open(os.path.join(temp_bids_path, "error_heudiconv.txt"), "a") as f:
                         print(f"WARNING: Subject {subj} was previously processed and will be skipped. Logged in error_heudiconv.txt")
                         f.write(str(datetime.datetime.now()) + "\t" + subj + " already processed\n")
         
         except Exception as e:
-            with open(os.path.join(bids_path, "error_heudiconv.txt"), "a") as f:
+            with open(os.path.join(temp_bids_path, "error_heudiconv.txt"), "a") as f:
                 print(f"WARNING: Unable to process subject {subj} due to an error. Logged in error_heudiconv.txt")
                 f.write(str(datetime.datetime.now()) + "\t" + subj + " error: " + str(e) + "\n")
             continue
 
     # .bidsignore file in case error_heudiconv.txt is created
-    if os.path.exists(os.path.join(bids_path, "error_heudiconv.txt")):
-        if not os.path.exists(os.path.join(bids_path, ".bidsignore")):
-            with open(os.path.join(bids_path, ".bidsignore"), "a") as f:
+    if os.path.exists(os.path.join(temp_bids_path, "error_heudiconv.txt")):
+        if not os.path.exists(os.path.join(temp_bids_path, ".bidsignore")):
+            with open(os.path.join(temp_bids_path, ".bidsignore"), "a") as f:
                 f.write("error_heudiconv.txt\n")
         else:
-            with open(os.path.join(bids_path, ".bidsignore"), "r+") as f:
+            with open(os.path.join(temp_bids_path, ".bidsignore"), "r+") as f:
                 lines = {line.rstrip() for line in f}
                 if "error_heudiconv.txt" not in lines:
                     f.write("\nerror_heudiconv.txt\n")   
 
     # Delete scans.tsv and events.tsv optional files                  
     ses_path = "ses-*" if use_sessions else ""
-    subses_path = os.path.join(bids_path, "sub-*", ses_path)
+    subses_path = os.path.join(temp_bids_path, "sub-*", ses_path)
 
     if len(list(Path(bids_path).glob(os.path.join("sub-*", ses_path, "*scans*")))) > 0:
         if delete_scans == True:
@@ -189,7 +187,7 @@ def main():
         else:
             print("WARNING: Invalid value for 'delete_scans' variable in heuristics file. No deletion of *_scans.tsv files was done.")
 
-    if len(list(Path(bids_path).glob(os.path.join("sub-*", ses_path, "func", "*_events.tsv")))) > 0:
+    if len(list(Path(temp_bids_path).glob(os.path.join("sub-*", ses_path, "func", "*_events.tsv")))) > 0:
         if delete_events == True:
             cmd_events = "rm " + os.path.join(subses_path, "func", "*_events.tsv")
             os.system(cmd_events)
