b86e66c7fe462d4e7af83b2ef179d0f8916604c9
arnau-guell <guell@ub.edu>
2025-10-29 02:51:30 +0100

    Update for new XA60 files + This major update allows handling images that come from Siemens XA60 scanners. They have to be exported in Enhanced mode and anonymized using the Service Use option.

--- 1-sort/Sort_DICOMS.py
+++ 1-sort/Sort_DICOMS.py
@@ -1,13 +1,80 @@
 ############################################
-########  SCRIPT FOR DICOM SORTING  ########
-########      BBSLab Mar 2024       ########
+#  SCRIPT FOR DICOM SORTING - XA60 UPDATED #
+#             BBSLab Oct 2025              #
 ############################################
 
 # Import libraries
+import shutil
 import os
 import sys
 from pathlib import Path
 import pydicom
+
+# Define functions
+def flatten_sub(root: Path):
+    root = Path(root).resolve()
+    changed = False
+
+    def get_dir_indices(path: Path):
+        """
+        For a given file path, return a list of numeric indices representing
+        the alphabetical position of each folder *only* if its parent
+        contains multiple subdirectories.
+        """
+        rel_parts = path.relative_to(root).parts[:-1]  # exclude filename
+        indices = []
+        current = root
+
+        for part in rel_parts:
+            subdirs = sorted([p.name for p in current.iterdir() if p.is_dir()])
+            if len(subdirs) > 1:  # only assign index if multiple siblings
+                idx = subdirs.index(part)
+                indices.append(idx)
+            current = current / part
+
+        return indices
+
+    # Move all files to root with renaming
+    for path in sorted(root.rglob('*')):
+        if not path.is_file():
+            continue
+        if path.parent == root:
+            continue  # already in root
+
+        indices = get_dir_indices(path)
+        prefix = '_'.join(map(str, indices)) + '_' if indices else ''
+        new_name = prefix + path.name
+        dest = root / new_name
+
+        # Avoid name collisions
+        count = 1
+        while dest.exists():
+            dest = root / f"{prefix}{count}_{path.name}"
+            count += 1
+
+        shutil.move(str(path), dest)
+        changed = True
+
+    # Remove empty dirs (bottom-up)
+    for dir_path in sorted(root.rglob('*'), key=lambda p: len(p.parts), reverse=True):
+        if dir_path.is_dir() and not any(dir_path.iterdir()):
+            dir_path.rmdir()
+            changed = True
+    
+    if changed:
+        print(f"INFO: {root.resolve()} directory was flattened before sorting")
+
+def get_sequence_name(dcminfo):
+    if dcminfo.get([0x18, 0x24]):
+        # Siemens E11
+        sequence_name = dcminfo[0x18, 0x24].value
+    elif dcminfo.get([0x18, 0x9005]):
+        # Siemens XA
+        sequence_name = dcminfo[0x18, 0x9005].value
+    else:
+        sequence_name = ""
+
+    return sequence_name
 
 # Set root directory
 root_dir = os.path.dirname(os.path.dirname(__file__))
@@ -19,20 +86,41 @@ meta_create()
 dicoms_path = meta_func("dicom", "the path to the DICOMs folder")  # Path to DICOM directories
 timepoint = meta_func("timepoint", "the name of the timepoint folder (e.g., 'TP2')") # Name of timepoint folder
 
+# Map for DICOM tags and directories
+tag2directory = {
+    '*fl2d1': 'Localizer',
+    '*tfl3d1_16ns': 'T1w_MPR',
+    '*tfl3d1_256ns': 'T1w_MPR',
+    '*spc_314ns': 'T2w_SPC',
+    'ep_b1495#12': 'dMRI',
+    'epse2d1_140': 'dMRI',
+    'ep_b_dMRI': 'dMRI',
+    '*tir2d1rs15': 'FLAIR',
+    'epse2d1_104': 'RESTING',
+    'epfid2d1_104': 'RESTING',
+    'tgse_mv3d1_2480': 'ASL_siemens',
+    'mbPCASL2d1_86': 'pCASL',
+    'epse2d1_86': 'pCASL',
+    '*fl3d1r_t70': 'TOF',
+    '*swi3d1r': 'SWI'
+}
+
 # Combine the DICOM path and the timepoint
 dicoms_to_order_folder = os.path.join(dicoms_path, timepoint)
 list_subjects = os.listdir(dicoms_to_order_folder)
 
-# Filter subjects to process based on the number of folders and files (fewer than 7 folders and more than 0 unsorted files, maintain only those which are not ordered)
+# Filter subjects to process based on the file structure
 list_subjects_to_do = [
-    s for s in list_subjects
-    if sum([
-        1 for file in os.listdir(os.path.join(dicoms_to_order_folder, s))
-        if os.path.isdir(os.path.join(dicoms_to_order_folder, s, file))
-    ]) < 7 and sum([
-        1 for file in os.listdir(os.path.join(dicoms_to_order_folder, s))
-        if os.path.isfile(os.path.join(dicoms_to_order_folder, s, file))
-    ]) > 0
+    subfolder.name for subfolder in Path(dicoms_to_order_folder).iterdir()
+    if subfolder.is_dir()
+    and (
+        any(f.is_file() for f in subfolder.iterdir())  # are there unsorted files directly in the sub folders?
+        or any(
+            f.is_file() and len(f.relative_to(subfolder).parts) >= 3  # aren't files in second-level folders?
+            for f in subfolder.rglob('*')
+        )
+    )
+    and not any(seq_folder.name == "T1w_MPR" for seq_folder in subfolder.rglob('*') if seq_folder.is_dir())  # isn't there a T1 folder?
 ]
 
 # Print summary of subjects to process
@@ -46,9 +134,20 @@ if list_subjects_to_do:
 for nSUB in list_subjects_to_do:
     print(f"Processing subject {nSUB}...")
     subject_folder = os.path.join(dicoms_to_order_folder, nSUB)
-    
+        
+    flatten_sub(subject_folder)
+
+    others_folder = os.path.join(subject_folder, r'OTHER')
+
+    if not os.path.exists(others_folder):
+        os.makedirs(others_folder)
+    if os.path.exists(os.path.join(subject_folder, 'DICOMDIR')):
+        os.rename(os.path.join(subject_folder, 'DICOMDIR'), os.path.join(others_folder, 'DICOMDIR'))
+    if os.path.exists(os.path.join(subject_folder, 'DICOM')):
+        shutil.rmtree(os.path.join(subject_folder, 'DICOM'))
+
     # List of all DICOM files
-    all_files_list = list(Path(subject_folder).glob(r'*.IMA'))
+    all_files_list = [f for f in Path(subject_folder).glob('*') if f.is_file()]
     
     # Initial count of files
     initial_files = sum([
@@ -56,61 +155,47 @@ for nSUB in list_subjects_to_do:
         if os.path.isfile(os.path.join(subject_folder, file))
     ])
 
-    # Map for DICOM tags and directories
-    tag2directory = {
-        '*fl2d1': 'Localizer',
-        '*tfl3d1_16ns': 'T1w_MPR',
-        '*spc_314ns': 'T2w_SPC',
-        'ep_b1495#12': 'dMRI',
-        'epse2d1_140': 'dMRI',
-        'ep_b_dMRI': 'dMRI',
-        '*tir2d1rs15': 'FLAIR',
-        'epse2d1_104': 'RESTING',
-        'epfid2d1_104': 'RESTING',
-        'tgse_mv3d1_2480': 'ASL_siemens',
-        'mbPCASL2d1_86': 'pCASL',
-        'epse2d1_86': 'pCASL',
-        '*fl3d1r_t70': 'TOF',
-        '*swi3d1r': 'SWI'
-    }
-
     # Sort files by acquisition time
     all_files_list.sort(key=os.path.getmtime)
 
     while all_files_list:
         # Read the first DICOM file
         this_dicom = pydicom.dcmread(all_files_list[0])
-        series_n = '%0.4d' % int(this_dicom.SeriesNumber)
 
-        # List of DICOMs with a given series number
-        dicoms_this_series = list(Path(subject_folder).glob(rf'*MR.{series_n}.*'))
+        if this_dicom.Modality != 'MR':
+            if not os.path.exists(others_folder):
+                os.makedirs(others_folder)
+            os.rename(all_files_list[0], os.path.join(others_folder, all_files_list[0].name))
+            # Redefine unsorted DICOM list and sort
+            all_files_list = [f for f in Path(subject_folder).glob('*') if f.is_file()]
+            all_files_list.sort(key=os.path.getmtime)
+            continue
 
+        sequence_name = get_sequence_name(this_dicom)
         # Determine sequence ID for diffusion
-        if this_dicom.SequenceName[0:4] == 'ep_b':
+        if sequence_name[0:4] == 'ep_b':
             sequence_id = 'ep_b_dMRI'
         else:
-            sequence_id = this_dicom.SequenceName # SequenceID for the rest
+            sequence_id = sequence_name # SequenceID for the rest
 
         # Move files to respective directories
         if sequence_id in tag2directory.keys():
             if not os.path.exists(os.path.join(subject_folder, tag2directory[sequence_id])):  
                 os.makedirs(os.path.join(subject_folder, tag2directory[sequence_id]))
-            for this_dicom_path in dicoms_this_series: # Move dicom to subdirectory
-                this_dicom_path = str(this_dicom_path)
-                new_dicom_path = this_dicom_path.replace(subject_folder, os.path.join(subject_folder, tag2directory[sequence_id]), 1)
-                os.rename(this_dicom_path, new_dicom_path)
+            this_dicom_path = str(all_files_list[0])
+            new_dicom_path = this_dicom_path.replace(subject_folder, os.path.join(subject_folder, tag2directory[sequence_id]), 1)
+            os.rename(this_dicom_path, new_dicom_path)
 
         # Same for Sequence_ID not in dictionary
         else:
-            if not os.path.exists(os.path.join(subject_folder, r'OTHER')):
-                os.makedirs(os.path.join(subject_folder, r'OTHER'))
-            for this_dicom_path in dicoms_this_series:
-                this_dicom_path = str(this_dicom_path)
-                new_dicom_path = this_dicom_path.replace(subject_folder, os.path.join(subject_folder, r'OTHER'), 1)
-                os.rename(this_dicom_path, new_dicom_path)
+            if not os.path.exists(others_folder):
+                os.makedirs(others_folder)
+            this_dicom_path = str(all_files_list[0])
+            new_dicom_path = this_dicom_path.replace(subject_folder, others_folder, 1)
+            os.rename(this_dicom_path, new_dicom_path)
 
         # Redefine unsorted DICOM list and sort
-        all_files_list = list(Path(subject_folder).glob(r'*.IMA'))
+        all_files_list = [f for f in Path(subject_folder).glob('*') if f.is_file()]
         all_files_list.sort(key=os.path.getmtime)
 
     # Info: Check if all files were sorted
